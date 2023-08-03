9bb6541073339238e80c82a745257dd23d8d342d
vinayakgarole <vinayakgarole10@gamil.com>
2023-08-03 15:15:35 +0530

    Practice DotNet

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Hello {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            // Console.WriteLine("Hello World");
+            // int a = 10;
+            // float b = 34.4f;
+            // bool isGreat = true;
+
+            // // Typecasting
+            // int x = (int) 3.5;
+            // Console.WriteLine(x);
+
+            // Console.WriteLine(a);
+            // Console.WriteLine(b);
+            // Console.WriteLine(isGreat);
+
+            // Console.WriteLine("Enter your name");
+            // string name = Console.ReadLine();
+            // Console.WriteLine("Hey hello " + name);
+
+            // Console.WriteLine("How many candies do you want?");
+            // string can = Console.ReadLine();
+            // Console.WriteLine("You will get 4 more candies " +
+            // (Convert.ToInt32(can) + 4));
+            // Console.ReadLine();
+
+            // int a = Math.Min(34, 345);
+            // double a = Math.Sqrt(36);
+            // Console.WriteLine(a);
+
+            string hello = "Hello world this is Vinayak";
+            Console.WriteLine(hello.Length);
+            Console.WriteLine(hello.ToUpper());
+            Console.WriteLine(hello.ToLower());
+        }
+    }
+}
